548de9e2ccfee71b8e85a637f92def348610f414
Emile <62842896+emidblol@users.noreply.github.com>
2024-05-05 17:20:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const h1 = document.getElementById("h1");
+const h1c = document.getElementById("h1c");
+h1.onclick = function () {
+  h1c.hidden = !h1c.hidden;
+};
+const dev = document.getElementById("dev");
+const devc = document.getElementById("devc");
+dev.onclick = function () {
+  devc.hidden = !devc.hidden;
+};
+const writer = document.getElementById("writer");
+const writerc = document.getElementById("writerc");
+writer.onclick = function () {
+  writerc.hidden = !writerc.hidden;
+};
+const pilot = document.getElementById("pilot");
+const pilotc = document.getElementById("pilotc");
+pilot.onclick = function () {
+  pilotc.hidden = !pilotc.hidden;
+};
